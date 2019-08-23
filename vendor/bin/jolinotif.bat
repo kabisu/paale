9a96262d1fcbd46469cafb3b0f941ad5eea3ebe5
Kabina Suwal <kabina.suwal92@gmail.com>
2019-08-23 18:08:40 +0545

    vendor deleted

--- vendor/bin/jolinotif.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-setlocal DISABLEDELAYEDEXPANSION
-SET BIN_TARGET=%~dp0/../jolicode/jolinotif/jolinotif
-php "%BIN_TARGET%" %*
--- vendor/bin/phpunit-watcher.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-setlocal DISABLEDELAYEDEXPANSION
-SET BIN_TARGET=%~dp0/../spatie/phpunit-watcher/phpunit-watcher
-php "%BIN_TARGET%" %*
